e54cd7aeab4af48ab2a375d4f19cf2658b63a73d
Your Name <you@example.com>
2022-08-17 22:14:40 -0700

    Added arrow right

---
+++ empathy_stories.js
@@ -0,0 +1,20 @@
+export const empathy_stories = [
+    {
+        title: "Dollar Tree store",
+        description_short: "Stella came with her mom into a store that everything is only one dollar...",
+        description_long: "",
+        thumbnail: "https://www.clipartmax.com/png/middle/422-4227601_dollar-store-products-from-vineland-syrup-dollar-store-transparent.png" 
+    },
+    {
+        title: "Lineal",
+        description_short: "Lineal is the only guard who takes care of the ranch...",
+        description_long: "",
+        thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT72exheX0ukejho9kJBPm02P-2g8X459TdOUB9eKq2WMnBDBkLN-V3sOcnD8hMc_jyo88&usqp=CAU" 
+    },
+    {
+        title: "A female biker finished 36 miles",
+        description_short: "Stella came with her mom into a store that everything is only one dollar...",
+        description_long: "",
+        thumbnail: "https://media.istockphoto.com/vectors/riding-a-bicycle-vector-id165670255?k=20&m=165670255&s=612x612&w=0&h=COxALPHxtKp9GlCC0vVIGm0M1j6AmllgwDzUaWnkZRs=" 
+    }
+]
