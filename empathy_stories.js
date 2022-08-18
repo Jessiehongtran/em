06131a8ccf88534ed90e2c76bc059f8a9d293c19
Your Name <you@example.com>
2022-08-18 11:37:44 -0700

    Added a page for all stories of empathy category

--- empathy_stories.js
+++ empathy_stories.js
@@ -19,5 +19,12 @@ export const empathy_stories = [
         description_short: "Stella came with her mom into a store that everything is only one dollar...",
         description_long: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
         thumbnail: "https://media.istockphoto.com/vectors/riding-a-bicycle-vector-id165670255?k=20&m=165670255&s=612x612&w=0&h=COxALPHxtKp9GlCC0vVIGm0M1j6AmllgwDzUaWnkZRs=" 
+    },
+    {
+        id: 4,
+        title: "A female biker finished 36 miles",
+        description_short: "Stella came with her mom into a store that everything is only one dollar...",
+        description_long: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
+        thumbnail: "https://media.istockphoto.com/vectors/riding-a-bicycle-vector-id165670255?k=20&m=165670255&s=612x612&w=0&h=COxALPHxtKp9GlCC0vVIGm0M1j6AmllgwDzUaWnkZRs=" 
     }
 ]
