5de40094cfdb2513b2eb8fdae7e8b4846911b890
Your Name <you@example.com>
2022-08-18 10:54:25 -0700

    Created detailed story page

--- empathy_stories.js
+++ empathy_stories.js
@@ -1,20 +1,23 @@
 export const empathy_stories = [
     {
+        id: 1,
         title: "Dollar Tree store",
         description_short: "Stella came with her mom into a store that everything is only one dollar...",
-        description_long: "",
+        description_long: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
         thumbnail: "https://www.clipartmax.com/png/middle/422-4227601_dollar-store-products-from-vineland-syrup-dollar-store-transparent.png" 
     },
     {
+        id: 2,
         title: "Lineal",
         description_short: "Lineal is the only guard who takes care of the ranch...",
-        description_long: "",
+        description_long: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
         thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT72exheX0ukejho9kJBPm02P-2g8X459TdOUB9eKq2WMnBDBkLN-V3sOcnD8hMc_jyo88&usqp=CAU" 
     },
     {
+        id: 3,
         title: "A female biker finished 36 miles",
         description_short: "Stella came with her mom into a store that everything is only one dollar...",
-        description_long: "",
+        description_long: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
         thumbnail: "https://media.istockphoto.com/vectors/riding-a-bicycle-vector-id165670255?k=20&m=165670255&s=612x612&w=0&h=COxALPHxtKp9GlCC0vVIGm0M1j6AmllgwDzUaWnkZRs=" 
     }
 ]
