30971bdd2a12b39c580b771ddd8679e26c28bac4
Your Name <you@example.com>
2022-08-22 16:17:00 -0700

    Got layout for round table page done

---
+++ js/rounds.js
@@ -0,0 +1,50 @@
+export const rounds = [
+    {
+        num: "#7890",
+        title: "Round Title",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+        time: "",
+        participants: [
+            {user_id: 1},
+            {user_id: 2},
+            {user_id: 3},
+            {user_id: 4}
+        ]
+    },
+    {
+        num: "#1234",
+        title: "Round Title",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+        time: "",
+        participants: [
+            {user_id: 2},
+            {user_id: 3},
+            {user_id: 4},
+            {user_id: 5}
+        ]
+    },
+    {
+        num: "#1245",
+        title: "Round Title",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+        time: "",
+        participants: [
+            {user_id: 2},
+            {user_id: 3},
+            {user_id: 4},
+            {user_id: 5}
+        ]
+    },
+    {
+        num: "#1267",
+        title: "Round Title",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+        time: "",
+        participants: [
+            {user_id: 2},
+            {user_id: 3},
+            {user_id: 4},
+            {user_id: 5}
+        ]
+    },
+]
