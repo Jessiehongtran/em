1dfbad84c3a55527c4551374f5c9fdc8bdfc84f9
Your Name <you@example.com>
2022-08-31 10:51:46 -0700

    added participants and link to join

--- js/rounds.js
+++ js/rounds.js
@@ -3,7 +3,8 @@ export const rounds = [
         num: 7890,
         title: "Round Title",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        time: "",
+        time: "12:00 08/31/2022",
+        link_to_join: "https://www.temporary-url.com/",
         participants: [
             {user_id: 1},
             {user_id: 2},
@@ -16,7 +17,8 @@ export const rounds = [
         num: 1234,
         title: "Round Title",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        time: "",
+        time: "12:00 08/31/2022",
+        link_to_join: "https://www.temporary-url.com/",
         participants: [
             {user_id: 2},
             {user_id: 3},
@@ -27,7 +29,8 @@ export const rounds = [
         num: 1245,
         title: "Round Title",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        time: "",
+        time: "12:00 08/31/2022",
+        link_to_join: "https://www.temporary-url.com/",
         participants: [
             {user_id: 4},
             {user_id: 5}
@@ -37,7 +40,8 @@ export const rounds = [
         num: 1267,
         title: "Round Title",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        time: "",
+        time: "12:00 08/31/2022",
+        link_to_join: "https://www.temporary-url.com/",
         participants: [
             {user_id: 2},
             {user_id: 3},
